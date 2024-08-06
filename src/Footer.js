4cf8eef3957d618f06b891b778506bb42c5c45df
ceciliaedv23 <ceciliaedv23@gmail.com>
2024-08-06 15:10:11 +0200

    Update Footer.js
    Update 2024-08-06

--- src/Footer.js
+++ src/Footer.js
@@ -3,23 +3,32 @@
 import React from "react";
 
 function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+
   return (
-    <footer id="footer">
-      <div>
-        <i class="fa-solid fa-lg fa-house" style={{ color: "#ffffff" }}></i>
-        <p>
-          <a href="https://ceciliaedv.netlify.app/">Min webbplats</a>
-        </p>
+    <footer id="footer" className="bg-black text-white">
+      <div className="flex flex-row p-6 gap-6">
+        <div className="flex flex-row items-center">
+          <i
+            className="fa-solid fa-lg fa-house px-2"
+            style={{ color: "#ffffff" }}
+          ></i>
+          <p>
+            <a href="https://ceciliaedv.netlify.app/">Min webbplats</a>
+          </p>
+        </div>
+        <div className="flex flex-row items-center">
+          <i
+            className="fa-solid fa-lg fa-envelope px-2"
+            style={{ color: "#ffffff" }}
+          ></i>
+          <p>
+            <a href="mailto:ceciliaedv23@gmail.com">Maila mig</a>
+          </p>
+        </div>
       </div>
-      <div>
-        <i class="fa-solid fa-lg fa-envelope" style={{ color: "#ffffff" }}></i>
-        <p>
-          <a href="mailto:ceciliaedv23@gmail.com">Maila mig</a>
-        </p>
-      </div>
-      <br />
-      <br />
-      <p>&#169; Cecilia Edvardsson 2023</p>
+      <p className="pt-2 pb-6 px-8">&#169; Cecilia Edvardsson {year}</p>
     </footer>
   );
 }
