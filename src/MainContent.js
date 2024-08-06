e4dd530c37ccb2b54a0d99f26f4124b3af0e250c
ceciliaedv23 <ceciliaedv23@gmail.com>
2024-08-06 15:11:02 +0200

    Update MainContent.js
    Update 2024-08-06

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,52 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Course from "./Course.js";
 import courselist from "./courselist.js";
+import About from "./About.js";
 
 function MainContent() {
-  const course2list = courselist.map((course) => {
-    return <Course course={course} />;
-  });
+  const sortedEarliest = [...courselist];
+  const sortedLatest = [...courselist].reverse();
+
+  const [currentList, setCurrentList] = useState(sortedEarliest);
+  const [latestActive, setLatestActive] = useState(false);
+
+  function sortLatest() {
+    setCurrentList(sortedLatest);
+    setLatestActive(true);
+  }
+
+  function sortEarliest() {
+    setCurrentList(sortedEarliest);
+    setLatestActive(false);
+  }
 
   return (
-    <div className="allt">
-      <div className="grid1">
-        <h2>Om webbplatsen</h2>
-        <div className="intro">
-          <p>
-            Här beskrivs de kurser som ingår i programmet Webbutveckling hos
-            Mittuniversitetet under läsperioden HT 2022 - VT 2024.
-            <br />
-            <br />
-            Jag som skapare av webbplatsen heter
-            <strong> Cecilia Edvardsson</strong> och är själv student på
-            programmet under denna tidsperiod.
-            <br />
-            <br />
-            Webbplatsen är skapad med React. Information om kurserna och bilder
-            är hämtade från en array i en fristående JavaScript-fil och inlagda
-            på sidan via props och metoden map().
-            <br />
-            <br />
-            <em className="mobile-only">
-              Klicka på den kurs du vill läsa mer om.
-            </em>
-          </p>
+    <div className="bg-gray-100">
+      <div className="mb-8 lg:mb-14 pl-8 pt-6 lg:pt-12 flex flex-col md:flex-row gap-4 bg-gray-100">
+        <p className="text-xl lg:text-2xl font-semibold">Sortera efter</p>
+        <p
+          onClick={sortEarliest}
+          className={`w-40 text-center rounded-lg font-medium p-2 cursor-pointer ${
+            latestActive ? "bg-blue-200" : "bg-blue-400"
+          }`}
+        >
+          Tidigast genomförd
+        </p>
+        <p
+          onClick={sortLatest}
+          className={`w-40 text-center rounded-lg font-medium p-2 cursor-pointer ${
+            latestActive ? "bg-blue-400" : "bg-blue-200"
+          }`}
+        >
+          Senast genomförd
+        </p>
+      </div>
+      <div className="">
+        <div className="flex flex-row gap-4 lg:gap-10 flex-wrap justify-center">
+          {currentList.map((course) => {
+            return <Course key={course.key} course={course} />;
+          })}
         </div>
+        <br />
+        <br />
+        <p className="w-40 bg-blue-800 m-auto text-center p-2 rounded-lg shadow-lg font-semibold text-white hover:bg-blue-700">
+          <a href="#top">Till sidans topp</a>
+        </p>
+
+        <About />
       </div>
-      <br />
-      <br />
-      <div className="grid2">{course2list}</div>
-      <br />
-      <br />
-      <div className="sticky">
-        <a href="#top">
-          <em
-            className="fa-solid fa-2xl fa-arrow-up"
-            style={{ color: "#000000" }}
-          ></em>
-        </a>
-      </div>
-      <br />
     </div>
   );
 }
